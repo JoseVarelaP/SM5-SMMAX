9570f34398e5d6b3ca21ed30185ba6ec44da46cc
JoseVarela <jo.lu6853@gmail.com>
2020-10-03 22:54:42 -0500

    Modulate Help text

--- BGAnimations/ScreenWithMenuElements overlay.lua
+++ BGAnimations/ScreenWithMenuElements overlay.lua
@@ -5,28 +5,5 @@ return Def.ActorFrame{
             self:align(0,1):y(SCREEN_BOTTOM)
         end
     },
-    
-    Def.HelpDisplay {
-		File="Common normal",
-		OnCommand=function(self)
-			self:zoom(0.5):zoomy(0):linear(0.5):zoomy(0.5):diffuseblink()
-		end,
-		InitCommand=function(self)
-            self:SetSecsBetweenSwitches( THEME:GetMetric("HelpDisplay","TipSwitchTime") )
-            local screenname = Var "LoadingScreen"
-            local UseSpecialText = function()
-                -- Title Menu uses special types based on the coin mode.
-                if screenname == "ScreenTitleMenu" then
-                    local CoinMode = GAMESTATE:GetCoinMode()
-                    return "HelpText".. ToEnumShortString( CoinMode )
-                end
-                return "HelpText"
-            end
-            self:SetTipsColonSeparated( THEME:GetString( Var "LoadingScreen" ,UseSpecialText()) )
-            self:xy( THEME:GetMetric( Var "LoadingScreen", "HelpX" ), THEME:GetMetric( Var "LoadingScreen", "HelpY" ) )
-		end,
-		OffCommand=function(self)
-			self:linear(0.5):zoomy(0)
-		end
-	}
+    loadfile( THEME:GetPathB("","SharedItems/HelpDisplay.lua") )()
 }
---
+++ BGAnimations/SharedItems/HelpDisplay.lua
@@ -0,0 +1,28 @@
+return Def.HelpDisplay {
+    File="Common normal",
+    OnCommand=function(self)
+        self:zoom(0.5):zoomy(0):linear(0.5):zoomy(0.5):diffuseblink()
+    end,
+    InitCommand=function(self)
+        self:SetSecsBetweenSwitches( THEME:GetMetric("HelpDisplay","TipSwitchTime") )
+        local screenname = Var "LoadingScreen"
+        local UseSpecialText = function()
+            -- Title Menu uses special types based on the coin mode.
+            if screenname == "ScreenTitleMenu" then
+                local CoinMode = GAMESTATE:GetCoinMode()
+                return "HelpText".. ToEnumShortString( CoinMode )
+            end
+            return "HelpText"
+        end
+        
+        local finaltext = THEME:GetString( Var "LoadingScreen", UseSpecialText() )
+        if finaltext == "" then
+            finaltext = THEME:GetString( "GenericScrollerLine", UseSpecialText() )
+        end
+        self:SetTipsColonSeparated( finaltext )
+        self:xy( THEME:GetMetric( Var "LoadingScreen", "HelpX" ), THEME:GetMetric( Var "LoadingScreen", "HelpY" ) )
+    end,
+    OffCommand=function(self)
+        self:linear(0.5):zoomy(0)
+    end
+}
